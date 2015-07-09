fdfc2f69832a1d51ef40e366b6d1b1651fcc202e
Sarah Loftis <sarahrloftis@gmail.com>
2015-07-09 15:45:53 -0700

    first draft

--- js/map.js
+++ js/map.js
@@ -1,32 +1,53 @@
 // Function to draw your map
-var drawMap() = function() {
-
-  // Create map and set viewd
- 
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+var drawMap = function() {
+	var map = L.map('container').setView([30,-100],4);
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+	getData(map);
 }
 
 // Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+var getData = function(map) {
+	$.ajax({
+		url:'data/response.json',
+		type: "get",
+		success:function(data) {
+			customBuild(map, data);
+		}, 
+		dataType:"json"
+	}) 
 }
 
-// Do something creative with the data here!  
-var customBuild = function() {
-
-  
+// Add points based on armed and fatalities 
+var customBuild = function(map, data) {
+	var unarmed_fatalities = L.layerGroup().addTo(map);
+	var unarmed_hits = L.layerGroup().addTo(map);
+	var armed_fatalities = L.layerGroup().addTo(map);
+	var armed_hits = L.layerGroup().addTo(map);
+	data.map(function(d){
+		if (d["Hit or Killed?"] == "Killed"){
+			if (d["Armed or Unarmed?"] == "Unarmed"){
+				var mark = new L.circleMarker([d.lat, d.lng], {fillColor:'red', radius:3, fillOpacity:.5, stroke:false});
+				unarmed_fatalities.addLayer(mark);
+			}else { //armed
+				var mark = new L.circleMarker([d.lat, d.lng], {fillColor:'purple', radius:3, fillOpacity:.5, stroke:false});
+				armed_fatalities.addLayer(mark);
+			}
+		}else { //hit}
+			if(d["Armed or Unarmed?"] == "Unarmed"){
+				var mark = new L.circleMarker([d.lat, d.lng], {fillColor:'blue', radius:3, fillOpacity:.5, stroke:false});
+				unarmed_hits.addLayer(mark);
+			}else {
+				var mark = new L.circleMarker([d.lat, d.lng], {fillColor:'black', radius:3, fillOpacity:.5, stroke:false});
+				armed_hits.addLayer(mark);
+			}
+		}
+		mark.bindPopup(d.Summary);
+	})
+	var overlays = {
+		"Unarmed fatalities" : unarmed_fatalities,
+		"Unarmed hits" : unarmed_hits,
+		"Armed fatalities" : armed_fatalities,
+		"Armed hits" : armed_hits
+	}
+	L.control.layers(null, overlays).addTo(map);
 }
-
-
